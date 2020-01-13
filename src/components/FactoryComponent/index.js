cfa26b3e59ebcf0f5aed333f502418e50ba0f67a
vlegkovskis <vladimirs.legkovskis@williamhill.com>
2020-01-13 11:38:24 +0100

    refactor 2

--- src/components/FactoryComponent/index.js
+++ src/components/FactoryComponent/index.js
@@ -4,10 +4,10 @@ import {componentConfig} from '../../data-client';
 function FactoryComponent({ data }) {
   const { type, children, props } = data;
   const Page = componentConfig[type];
-  const components = children.map(component => {
+  const views = children.map(component => {
     const View = componentConfig[component.type];
     const pProps = component.props;
-    const parentChildren = !component.children ? [] : component.children.map((child) => {
+    const components = !component.children ? [] : component.children.map((child) => {
       const Child = componentConfig[child.type];
       const cProps = child.props;
       return(
@@ -19,13 +19,13 @@ function FactoryComponent({ data }) {
         key={component.id}
         {...pProps}
       >
-        {parentChildren.map(view => view)}
+        {components.map(view => view)}
       </View>
     )
   });
   return(
     <Page {...props}>
-      {components.map(content => content)}
+      {views.map(content => content)}
     </Page>
   )
 }
